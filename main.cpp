ad61fb21a8d7a887c7e14a1fc489390014650e95
Chirag Juneja <chiragjuneja6@gmail.com>
2024-04-13 15:16:32 +0530

    opencv link

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include<iostream>
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 int main(){
-    std::cout << "Hello World" << std::endl;
+    std::string image_path = cv::samples::findFile("./assets/test.jpg");
+    cv::Mat img = cv::imread(image_path,cv::IMREAD_COLOR);
+    std::cout << image_path <<std::endl;        
 }
-
