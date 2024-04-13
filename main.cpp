a2a50c32ee35971f0dca23593a4e8a72fa1582ec
Chirag Juneja <chiragjuneja6@gmail.com>
2024-04-13 16:27:23 +0530

    onnxruntime link

--- main.cpp
+++ main.cpp
@@ -2,9 +2,30 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <onnxruntime/core/session/onnxruntime_cxx_api.h>
+//#include <onnxruntime_cxx_api.h>
 
 int main(){
+    // Initalize OpenCV
+    
     std::string image_path = cv::samples::findFile("./assets/test.jpg");
     cv::Mat img = cv::imread(image_path,cv::IMREAD_COLOR);
-    std::cout << image_path <<std::endl;        
+    
+    std::cout << image_path << "Is Empty? "<< img.empty() <<std::endl;        
+    
+    // Initialize OnnxRuntime
+    std::string modelPath = "./assets/mnist.onnx";
+
+    Ort::Env env;
+    Ort::SessionOptions sessionOptions{nullptr};
+    Ort::Session session{env, modelPath.c_str(), sessionOptions};
+
+    Ort::MemoryInfo memoryInfo = Ort::MemoryInfo::CreateCpu(
+            OrtAllocatorType::OrtArenaAllocator,
+            OrtMemType::OrtMemTypeDefault);
+
+    auto allocator = new Ort::Allocator(session,memoryInfo);
+
+    std::cout<< session.GetInputNameAllocated(0,*allocator).get() << std::endl;    
+    std::cout<< session.GetOutputNameAllocated(0,*allocator).get() << std::endl;    
 }
